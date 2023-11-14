2dfcc2b79ee360e11dbcd939e16caecc2b8a36cd
DVMaja <majadreilinger@gmail.com>
2023-11-14 15:58:50 +0100

    api routok jók

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\UserController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -17,3 +18,12 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::apiResource('/users', UserController::class);
+Route::apiResource('/api/users', UserController::class);
+
+
+Route::get('/user', [UserController::class, 'edit'])->name('user.edit');
+Route::post('/user', [UserController::class, 'update'])->name('user.update');
+Route::patch('/user', [UserController::class, 'update'])->name('user.update');
+Route::delete('/user', [UserController::class, 'destroy'])->name('user.destroy');
